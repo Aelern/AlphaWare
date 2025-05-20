d6187e4255c0a7072f23fd274d743bfc8e0550c9
Andrew Boyce <akboyce72@gmail.com>
2025-05-20 15:35:53 -0400

    Started working on bowman animations

--- Actors/Maple/bowman.gd
+++ Actors/Maple/bowman.gd
@@ -2,10 +2,11 @@ class_name Bowman
 extends CharacterBody2D
 
 @export var top_speed = 200
-@export var acceleration = 25
-@export var jump_speed = 400
-@export var gravity = 50
-@export var terminal_velocity = 300
+@export var acceleration = 2000
+@export var deceleration = 5000
+@export var jump_speed = 390
+@export var gravity = 1200
+@export var terminal_velocity = 1200
 
 var own_speed: Vector2
 var facing_right = false
@@ -23,26 +24,38 @@ func _physics_process(delta: float) -> void:
 			attack_state(delta)
 		States.HITSTUN:
 			hitstun_state(delta)
+	move_and_slide()
 
 func grounded_state(delta: float):
-	print("Ayy I'm walkin' here")
 	if not is_on_floor():
 		player_state = States.AIR
+	var dir = 0
 	if Input.is_action_pressed("left"):
-		own_speed.x = clamp(own_speed.x - acceleration, top_speed * -1, top_speed)
+		dir = dir - 1
 	if Input.is_action_pressed("right"):
-		own_speed.x = clamp(own_speed.x + acceleration, top_speed * -1, top_speed)
+		dir = dir + 1
+	if dir != 0:
+		own_speed.x = clamp(own_speed.x + acceleration * dir * delta, top_speed * -1, top_speed)
+		if dir == 1 and not facing_right:
+			facing_right = true
+		elif dir == -1 and facing_right:
+			facing_right = false		
+	else:
+		if own_speed.x > 0:
+			own_speed.x = clamp(own_speed.x - deceleration * delta, 0, top_speed)
+		elif own_speed.x < 0:
+			own_speed.x = clamp(own_speed.x + deceleration * delta, top_speed * -1, 0)
 	if Input.is_action_just_pressed("action"):
 		own_speed.y = jump_speed * -1
 		player_state = States.AIR
 	velocity = own_speed
 
 func air_state(delta: float):
-	print("Yoooooooooooooop")
-	own_speed.y = clamp(own_speed.y + gravity, jump_speed * -2, terminal_velocity)
+	own_speed.y = clamp(own_speed.y + (gravity * delta), jump_speed * -2, terminal_velocity)
 	if is_on_floor():
 		player_state = States.GROUND
 		own_speed.y = 0
+	velocity = own_speed
 
 func attack_state(delta: float):	
 	print("Watch out, I'm shooting at you")
